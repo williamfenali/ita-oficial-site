e7b1a6acded2467a08ba19603d6262f38800f47a
William Fenali <113377453+williamfenali@users.noreply.github.com>
2023-04-27 07:58:24 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open')
+}
+
+const sr = ScrollReveal({
+    distance: '65px',
+    duration: 2600,
+    delay: 450,
+    reset: true
+});
+
+sr.reveal('.content-text', {delay: 200, origin: 'top'});
+sr.reveal('.content-img', {delay: 450, origin: 'top'});
+sr.reveal('.logo', {delay: 550, origin: 'left'});
